01f52261cb3cc8ad6953e1e4960b669c13444808
MinhDuc9 <minhduc0567@gmail.com>
2023-11-15 00:10:56 +1100

    Last update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const methodOverride = require('method-override') 
+const methodOverride = require('method-override');
 const handlebars = require('express-handlebars').engine;
 const app = express();
 const port = 3000;
 
-const SortMiddleware = require('./app/controllers/middleware/SortMiddleware')
+const SortMiddleware = require('./app/controllers/middleware/SortMiddleware');
 
 const route = require('./routes');
 const db = require('./config/db');
@@ -14,12 +14,14 @@ const db = require('./config/db');
 // Connect to DB
 db.connect();
 
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 app.use(express.json());
 
 app.use(methodOverride('_method'));
@@ -32,38 +34,38 @@ app.use(SortMiddleware);
 
 // Template engine
 app.engine(
-  'hbs', 
-  handlebars({
-    extname: '.hbs',
-    helpers: {
-      sum: (a, b) => a + b,
-      sortable: (field, sort) => {
-        const sortType = field === sort.column ? sort.type : 'default';
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
 
-        const icons = {
-          default: 'oi oi-elevator',
-          asc: 'oi oi-sort-ascending',
-          desc: 'oi oi-sort-descending',
-        };
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending',
+                };
 
-        const types = {
-          default: 'desc',
-          asc: 'desc',
-          desc: 'asc',
-        }
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
 
-        const icon = icons[sortType];
-        const type = types[sortType];
+                const icon = icons[sortType];
+                const type = types[sortType];
 
-        return `<a href="?_sort&column=${field}&type=${type}">
+                return `<a href="?_sort&column=${field}&type=${type}">
           <span class="${icon}"></span>
         </a>`;
-      }
-    }
-  })
+            },
+        },
+    })
 );
 
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
@@ -71,5 +73,5 @@ route(app);
 
 // 127.0.0.1 - localhost
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
